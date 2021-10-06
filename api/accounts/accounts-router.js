25a81b7aecfc18d48d5675a508b207b0fce1d548
BrendonHawkins <artbitcodedata@gmail.com>
2021-10-06 10:41:00 -0400

    completed

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -19,12 +19,17 @@ router.get('/:id', md.checkAccountId, async (req, res, next) => {
 })
 
 router.post(
-  '/', 
+'/', 
   md.checkAccountPayload,
    md.checkAccountNameUnique,
-    (req, res, next) => {
+   async (req, res, next) => {
   try{
-    res.json('post account')
+    const newAccount = await Account.create({
+       name: req.body.name.trim(),
+       budget: req.body.budget
+        
+    })
+    res.status(201).json(newAccount)
   }
   catch(err) {
     next(err)
@@ -33,21 +38,22 @@ router.post(
 
 router.put('/:id',
 md.checkAccountPayload,
-md.checkAccountNameUnique,
 md.checkAccountId,
- (req, res, next) => {
+ async (req, res, next) => {
   try{
-
+   const updated = await Account.updateById(req.params.id, req.body)
+   res.status(200).json(updated)
   }
   catch(err) {
     next(err)
   }
 });
 
-router.delete('/:id', md.checkAccountId,  (req, res, next) => {
+router.delete('/:id', md.checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
   try{
-
+    await Account.deleteById(req.params.id)
+    res.json(req.account)
   }
   catch(err) {
     next(err)
@@ -55,7 +61,7 @@ router.delete('/:id', md.checkAccountId,  (req, res, next) => {
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  res.status(err.status || 5000).json({
+  res.status(err.status || 500).json({
     message: err.message,
   })
 })
